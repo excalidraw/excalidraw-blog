7ba3436b5c788e3b42f8316a2a2679ec173e92b1
Lipis <lipiridis@gmail.com>
2020-03-22 05:44:02 +0200

    Add commit hooks (#12)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
 // custom typefaces
 import "typeface-montserrat";
 import "typeface-merriweather";
-require("prismjs/themes/prism-solarizedlight.css")
+require("prismjs/themes/prism-solarizedlight.css");
