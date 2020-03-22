ab2cf1d9085fad31752f40fc8fcb08843e7120a3
Panayiotis Lipiridis <lipiridis@gmail.com>
2020-03-22 03:25:30 +0200

    Add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
-        //trackingId: "ADD YOUR TRACKING ID HERE",
+        trackingId: "UA-387204-13",
       },
     },
     "gatsby-plugin-feed",
