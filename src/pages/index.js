63354adcb42294ce1519dac4ce2ebdaf628c0cca
Jed Fox <git@twopointzero.us>
2020-03-24 07:51:04 -0400

    Add support for external articles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,43 +1,83 @@
 import React from "react";
 import { Link, graphql } from "gatsby";
+import moment from "moment";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import { rhythm } from "../utils/typography";
 
+import linkPosts from "../../content/links.yml";
+
 function BlogIndex({ data, location }) {
-  const posts = data.allMarkdownRemark.edges;
+  const posts = data.allMarkdownRemark.edges.map((edge) => edge.node);
   const title = data.site.siteMetadata.title;
+
+  const allPosts = posts
+    .map((p) => ({ ...p, date: p.frontmatter.rawDate }))
+    .concat(linkPosts)
+    .sort((a, b) => (a.date < b.date ? 1 : a.date > b.date ? -1 : 0));
+  console.log(allPosts.map((p) => p.date).sort());
 
   return (
     <Layout location={location} title={title}>
       <SEO title="All posts" />
-      {posts.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug;
-        return (
-          <div key={node.fields.slug}>
-            <h3
-              style={{
-                marginBottom: rhythm(1 / 4),
-              }}
-            >
-              <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                {title}
-              </Link>
-            </h3>
-            <p>
-              <strong>{node.frontmatter.date}</strong>
-              {" — "}
-              <span style={{ opacity: 0.75 }}>{node.excerpt}</span>
-            </p>
-          </div>
-        );
-      })}
+      {allPosts.map((post) =>
+        post.frontmatter ? (
+          <Post post={post} key={post.fields.slug} />
+        ) : (
+          <LinkPost post={post} key={post.title} />
+        )
+      )}
     </Layout>
   );
 }
 
 export default BlogIndex;
+
+function Post({ post }) {
+  const title = post.frontmatter.title || post.fields.slug;
+  return (
+    <div>
+      <h3
+        style={{
+          marginBottom: rhythm(1 / 4),
+        }}
+      >
+        <Link style={{ boxShadow: `none` }} to={post.fields.slug}>
+          {title}
+        </Link>
+      </h3>
+      <p>
+        <strong>{post.frontmatter.date}</strong>
+        {" — "}
+        <span style={{ opacity: 0.75 }}>{post.excerpt}</span>
+      </p>
+    </div>
+  );
+}
+
+function LinkPost({ post }) {
+  return (
+    <div>
+      <h3
+        style={{
+          marginBottom: rhythm(1 / 4),
+        }}
+      >
+        <a style={{ boxShadow: `none` }} href={post.url}>
+          {post.title}
+        </a>
+      </h3>
+      <p>
+        <strong>{moment(post.date).format("MMMM DD, YYYY")}</strong>
+        {" — "}
+        <em style={{ opacity: 0.75 }}>External post</em>
+        {" — "}
+        <span style={{ opacity: 0.75 }}>{post.excerpt}</span>
+      </p>
+    </div>
+  );
+}
 
 export const pageQuery = graphql`
   query BlogIndex {
@@ -55,6 +95,7 @@ export const pageQuery = graphql`
             slug
           }
           frontmatter {
+            rawDate: date
             date(formatString: "MMMM DD, YYYY")
             title
           }
