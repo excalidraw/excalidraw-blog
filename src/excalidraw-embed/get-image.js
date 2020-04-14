f2d57e87902e35577c8253b0ded6b4e829eec0b8
Jed Fox <git@twopointzero.us>
2020-04-14 10:34:11 -0400

    Fix rendering images (#74)
    * Hopefully fix rendering images
    
    * test
    
    * Fixes

--- src/excalidraw-embed/get-image.js
+++ src/excalidraw-embed/get-image.js
@@ -12,21 +12,18 @@ module.exports = async (url) => {
   const frame = await page.mainFrame();
   const result = await frame.evaluate(
     () =>
-      new Promise((resolve, error) => {
+      new Promise((resolve, reject) => {
         try {
-          const button = document.querySelector('[aria-label="Export to SVG"]');
-          const link = document.createElement("a");
           delete window.chooseFileSystemEntries;
-          document.createElement = () => link;
-          link.click = () => {
-            fetch(link.href)
-              .then((res) => res.text())
-              .then(resolve)
-              .catch(error);
-          };
+          const reader = new FileReader();
+          reader.addEventListener("loadend", () => resolve(reader.result));
+          reader.addEventListener("error", () => reject(reader.error));
+          URL.createObjectURL = (blob) => reader.readAsText(blob);
+
+          const button = document.querySelector('[aria-label="Export to SVG"]');
           button.click();
-        } catch (e) {
-          error(e);
+        } catch (error) {
+          reject(error);
         }
       })
   );
